149a61ba2f5064a3e8697b2e2d8fce6739377ae2
MoneyBagCoin <moneybagcoins@gmail.com>
2018-02-01 01:42:12 +0100

    Added missing \n

--- src/util.cpp
+++ src/util.cpp
@@ -1175,7 +1175,7 @@ void createConf()       //Automatic MoneyBagCoin.conf generation
 
     ofstream pConf;
     pConf.open(GetConfigFile().generic_string().c_str());
-    const char* nodes =  "port=43210"
+    const char* nodes =  "\nport=43210"
                         "\nrpcallowip=127.0.0.1"
                         "\ndaemon=1"
                         "\nserver=1"
